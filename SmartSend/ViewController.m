7981a1f221acf3d93af3f5a1678434ff572918de
Michael Bishoff <bishoff1@umbc.edu>
2014-02-15 14:22:13 -0500

    Initial Commit

---
+++ SmartSend/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SmartSend
+//
+//  Created by Michael Bishoff on 2/15/14.
+//  Copyright (c) 2014 Michael Bishoff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
